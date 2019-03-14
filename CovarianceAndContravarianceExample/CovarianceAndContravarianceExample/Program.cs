fb96cd86677e0ee381ed82fbee862d29c4cb57eb
dchekushin <denis.chekushin@gmail.com>
2019-03-14 12:53:36 +0300

    Covariance and contravariance example

---
+++ CovarianceAndContravarianceExample/CovarianceAndContravarianceExample/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CovarianceAndContravarianceExample
+{
+    abstract class Engine
+    {
+        
+    }
+
+    class V8Engine : Engine
+    {
+        
+    }
+
+    interface ICar<out T> where T : Engine
+    {
+        T GetEngine();
+    }
+
+    class Bmw : ICar<V8Engine>
+    {
+        public V8Engine GetEngine()
+        {
+            return new V8Engine();
+        }
+    }
+    
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Bmw bmw = new Bmw();
+            
+            ICar<V8Engine> vEightCar = bmw;
+            
+            ICar<Engine> someCar = bmw;
+        }
+    }
+}
