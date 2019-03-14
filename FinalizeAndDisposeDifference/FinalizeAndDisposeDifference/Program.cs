54027867b0d80eeef60cbf928ce652962cfc4a3f
dchekushin <denis.chekushin@gmail.com>
2019-03-14 12:52:56 +0300

    Finalize and dispose difference

---
+++ FinalizeAndDisposeDifference/FinalizeAndDisposeDifference/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FinalizeAndDisposeDifference
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                using (MyDisposableClass x = new MyDisposableClass(i))
+                {
+                    
+                }
+            }
+        }
+    }
+
+    public class MyDisposableClass : IDisposable
+    {
+        public MyDisposableClass(int id)
+        {
+            Id = id;
+            Console.WriteLine($"Call Constructor {Id}");
+        }
+
+        private int Id { get; }
+        
+        ~MyDisposableClass()
+        {
+            ReleaseUnmanagedResources();
+        }
+
+        private void ReleaseUnmanagedResources()
+        {
+            Console.WriteLine($"Call Finalize {Id}");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"Call Dispose {Id}");
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+    }
+    
+}
